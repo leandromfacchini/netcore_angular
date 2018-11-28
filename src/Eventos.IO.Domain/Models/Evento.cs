23fb5adfb16e26e7692a73f921c96ca2a599bbeb
Leandro <leandromfacchini@gmail.com>
2018-11-27 23:13:07 -0200

    Inicio de projeto

---
+++ src/Eventos.IO.Domain/Models/Evento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Models
+{
+    public class Evento
+    {
+        public Guid Id { get; set; }
+
+    }
+}
