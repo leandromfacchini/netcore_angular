ddf43373c95029244aeb163ae4e223bd66f63567
Leandro <leandromfacchini@gmail.com>
2018-11-28 15:26:14 -0200

    Adicionado FluentValidation

--- src/Eventos.IO.Domain.Core/Models/Entity.cs
+++ src/Eventos.IO.Domain.Core/Models/Entity.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
 
 namespace Eventos.IO.Domain.Core.Models
 {
-    public abstract class Entity
+    public abstract class Entity<T> : AbstractValidator<T> where T : class
     {
+        protected Entity()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
         public Guid Id { get; protected set; }
+        public ValidationResult ValidationResult { get; protected set; }
+
+        public abstract bool EhValido();
 
         public override bool Equals(object obj)
         {
-            var compareTo = obj as Entity;
+            var compareTo = obj as Entity<T>;
 
             if (ReferenceEquals(this, compareTo)) return true;
             if (ReferenceEquals(null, compareTo)) return false;
@@ -18,7 +26,7 @@ namespace Eventos.IO.Domain.Core.Models
             return Id.Equals(compareTo.Id);
         }
 
-        public static bool operator ==(Entity a, Entity b)
+        public static bool operator ==(Entity<T> a, Entity<T> b)
         {
             if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
                 return true;
@@ -29,7 +37,7 @@ namespace Eventos.IO.Domain.Core.Models
             return a.Equals(b);
         }
 
-        public static bool operator !=(Entity a, Entity b)
+        public static bool operator !=(Entity<T> a, Entity<T> b)
         {
             return !(a == b);
         }
