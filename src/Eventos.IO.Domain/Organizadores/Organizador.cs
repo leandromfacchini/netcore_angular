e6701b1a99ce0fbccf969a9c845136f77ed28aaf
Leandro <leandromfacchini@gmail.com>
2018-12-05 07:37:03 -0200

    Trabalhando camada de dados.

--- src/Eventos.IO.Domain/Organizadores/Organizador.cs
+++ src/Eventos.IO.Domain/Organizadores/Organizador.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using Eventos.IO.Domain.Core.Models;
+using Eventos.IO.Domain.Eventos;
 
 namespace Eventos.IO.Domain.Organizadores
 {
     public class Organizador : Entity<Organizador>
     {
-        public Organizador(Guid id)
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
+
+        public virtual ICollection<Evento> Eventos { get; set; }
+
+        public Organizador(Guid id, string nome, string cpf, string email)
         {
             Id = id;
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
         }
+
+        //EF Construtor
+        public Organizador() { }
 
         public override bool EhValido()
         {
-            throw new System.NotImplementedException();
+            return true;
         }
+
     }
 }
