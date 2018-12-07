a0b065cd6377b67a8b4468a5d2a5929a4a2817d4
Leandro <leandromfacchini@gmail.com>
2018-12-07 07:03:19 -0200

    Adicionado projeto Application.

---
+++ src/Eventos.IO.Application/ViewModels/EventoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Eventos.IO.Application.ViewModels
+{
+    public class EventoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome é requerido")]
+        [MinLength(2, ErrorMessage = "O tamanho mínimo do Nome é {1}")]
+        [MaxLength(150, ErrorMessage = "O tamanho máximo do Nome é {1}")]
+        [Display(Name = "Nome do Evento")]
+        public string Nome { get; set; }
+
+    }
+}
