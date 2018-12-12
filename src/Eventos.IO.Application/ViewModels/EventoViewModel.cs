d379ffddd1911907d034f997108ccdf7088b807e
Leandro <leandromfacchini@gmail.com>
2018-12-11 23:05:56 -0200

    Adicionadas classes de ViewModel

--- src/Eventos.IO.Application/ViewModels/EventoViewModel.cs
+++ src/Eventos.IO.Application/ViewModels/EventoViewModel.cs
@@ -16,5 +16,38 @@ namespace Eventos.IO.Application.ViewModels
         [Display(Name = "Nome do Evento")]
         public string Nome { get; set; }
 
+        [Display(Name = "Descrição curta do Evento")]
+        public string DescricaoCurta { get; set; }
+
+        [Display(Name = "Descrição longa do Evento")]
+        public string DescricaoLonga { get; set; }
+
+        [Display(Name = "Início do Evento")]
+        [Required(ErrorMessage = "A data é requerida")]
+        public DateTime DataInicio { get; set; }
+
+        [Display(Name = "Fim do Evento")]
+        [Required(ErrorMessage = "A data é requerida")]
+        public DateTime DataFim { get; set; }
+
+        [Display(Name = "Será Gratuito ?")]
+        public bool Gratuito { get; set; }
+
+        [Display(Name = "Valor")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DataType(DataType.Currency, ErrorMessage = "Moeda em formato inválido")]
+        public decimal Valor { get; set; }
+
+        [Display(Name = "Será Online ?")]
+        public bool Online { get; set; }
+
+        [Display(Name = "Empresa / Grupo Organizador")]
+        public string NomeEmpresa { get; set; }
+
+        public EnderecoViewModel Endereco { get; set; }
+        public CategoriaViewModel Categoria { get; set; }
+        public Guid CategoriaId { get; set; }
+        public Guid OrganizadorId { get; set; }
+
     }
 }
