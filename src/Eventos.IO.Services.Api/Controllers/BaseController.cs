7a6c3676e9524c71ca4dd6c7f01b068903422906
Leandro Facchini <leandromfacchini@gmail.com>
2019-03-06 11:17:31 -0300

    Adicionado projeto Api

---
+++ src/Eventos.IO.Services.Api/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventos.IO.Services.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Base")]
+    public class BaseController : Controller
+    {
+    }
+}
